7223e5b0a7d8d60a3c55c073bc5755cf01a0a5d1
Victor Gaudaire <victorgaud@protonmail.com>
2025-02-08 16:52:08 +0100

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/marie/', // Remplace <nom-de-ton-depot> par le nom réel de ton dépôt
+  base: '/src/', // Remplace <nom-de-ton-depot> par le nom réel de ton dépôt
 });
